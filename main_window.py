bd078e4ef33c391761416958f5e3106664fa88eb
christopher-pisz-fivestars <christopher.pisz@fivestars.com>
2019-07-09 18:38:32 -0500

    [] Trim it down
    [](https://fivestars.atlassian.net/browse/)

--- main_window.py
+++ main_window.py
@@ -1,8 +1,5 @@
 import threading
-import windows_hooks
 import wx
-
-from constants import InputTypes
 
 
 class MainWindow(wx.Frame):
@@ -49,50 +46,22 @@ class MainPanel(wx.Panel):
 
         wx.Panel.__init__(self, parent)
         self.textbox = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY)
-        self.button_task = wx.Button(self, label="Long Task")
-        self.button_consume = wx.Button(self, label="Start Consuming")
 
         self.horizontal = wx.BoxSizer()
         self.horizontal.Add(self.textbox, proportion=1, flag=wx.EXPAND)
 
-        self.horizontal2 = wx.BoxSizer()
-        self.horizontal2.Add(self.button_consume)
-        self.horizontal2.Add(self.button_task)
-
         self.sizer_vertical = wx.BoxSizer(wx.VERTICAL)
         self.sizer_vertical.Add(self.horizontal, proportion=1, flag=wx.EXPAND)
-        self.sizer_vertical.Add(self.horizontal2, proportion=1, flag=wx.CENTER)
         self.SetSizerAndFit(self.sizer_vertical)
 
-        self.Bind(wx.EVT_BUTTON, self.on_click_task, self.button_task)
-        self.Bind(wx.EVT_BUTTON, self.on_click_consume, self.button_consume)
-
-    def on_click_task(self, event):
-        self.textbox.AppendText('Starting a long task...\n')
-
-    def on_click_consume(self, event):
-        if not self.consuming:
-            self.button_consume.SetLabel('Stop Consuming')
-            self.hook_manager.set_user_input_types_to_consume(InputTypes.MOUSE_BUTTONS)
-            self.consuming = True
-
-        else:
-            self.button_consume.SetLabel('Start Consuming')
-            self.hook_manager.set_user_input_types_to_consume(InputTypes.NONE)
-            self.consuming = False
-
-    def print_to_text_box(self, hook_event_type, event):
+    def print_to_text_box(self, event):
         print "Printing message on Thread with Id {}".format(threading.current_thread().ident)
-        print 'Event type: {}'.format(hook_event_type)
-
-        tokens = hook_event_type.split('.')
-        if tokens[1] == 'mouse':
-            self.textbox.AppendText('MessageName: {}\n'.format(event.MessageName))
-            self.textbox.AppendText('Message: {}\n'.format(event.Message))
-            self.textbox.AppendText('Time: {}\n'.format(event.Time))
-            self.textbox.AppendText('Window: {}\n'.format(event.Window))
-            self.textbox.AppendText('WindowName: {}\n'.format(event.WindowName))
-            self.textbox.AppendText('Position: {}\n'.format(event.Position))
-            self.textbox.AppendText('Wheel: {}\n'.format(event.Wheel))
-            self.textbox.AppendText('Injected: {}\n'.format(event.Injected))
-            self.textbox.AppendText('---\n')
+        self.textbox.AppendText('MessageName: {}\n'.format(event.MessageName))
+        self.textbox.AppendText('Message: {}\n'.format(event.Message))
+        self.textbox.AppendText('Time: {}\n'.format(event.Time))
+        self.textbox.AppendText('Window: {}\n'.format(event.Window))
+        self.textbox.AppendText('WindowName: {}\n'.format(event.WindowName))
+        self.textbox.AppendText('Position: {}\n'.format(event.Position))
+        self.textbox.AppendText('Wheel: {}\n'.format(event.Wheel))
+        self.textbox.AppendText('Injected: {}\n'.format(event.Injected))
+        self.textbox.AppendText('---\n')
--- windows_hooks.py
+++ windows_hooks.py
@@ -1,34 +1,10 @@
 import pyHook
 import threading
 import pythoncom
-import win32con
-import wx
-
-from pyHook import HookConstants
-from constants import HookEvents, InputTypes
-
-MOUSE_BUTTON_EVENTS = {HookConstants.WM_LBUTTONDOWN, HookConstants.WM_LBUTTONUP,
-                       HookConstants.WM_RBUTTONDOWN, HookConstants.WM_RBUTTONUP}
-HOOK_EVENT_MAP = {HookConstants.WM_LBUTTONDOWN: HookEvents.mouse_left_down.value,
-                  HookConstants.WM_LBUTTONUP: HookEvents.mouse_left_up,
-                  HookConstants.WM_RBUTTONDOWN: HookEvents.mouse_right_down.value,
-                  HookConstants.WM_RBUTTONUP: HookEvents.mouse_right_up,
-                  HookConstants.WM_MOUSEMOVE: HookEvents.mouse_move.value}
-
-WX_EVT_MOUSE_LEFT_BUTTON_DOWN_ID = wx.NewId()
 
 
 class WindowsHooksWrapper(object):
-    """
-    Provides a means to subscribe to keyboard and mouse events via Windows Hooks
-    """
     def __init__(self):
-        self.consuming_user_keyboard_events = False
-        self.consuming_user_mouse_button_events = False
-        self.consuming_user_mouse_move_events = False
-        self.publishing_user_keyboard_events = True
-        self.publishing_user_mouse_button_events = True
-        self.publishing_user_mouse_move_events = True
         self.hook_manager = None
         self.started = False
         self.thread = threading.Thread(target=self.thread_proc)
@@ -53,66 +29,6 @@ class WindowsHooksWrapper(object):
         self.started = False
         self.thread.join()
 
-    def set_user_input_types_to_consume(self, input_types_to_consume):
-        """
-        Tells the windows hooks wrapper to consume user input events or not.
-        Consumed events will not be passed to other hooks or the process they were intended for.
-        Injected events will always be passed on.
-        :param input_types_to_consume: Flag. Some bitwise combination of the InputTypes enum that
-            represents each type of input and whether to consume user events of that type or not
-        """
-        self.consuming_user_keyboard_events = (input_types_to_consume & InputTypes.KEYBOARD)
-        self.consuming_user_mouse_button_events = \
-            (input_types_to_consume & InputTypes.MOUSE_BUTTONS)
-        self.consuming_user_mouse_move_events = (input_types_to_consume & InputTypes.MOUSE_MOVE)
-
-    def set_user_input_types_to_publish(self, input_types_to_publish):
-        """
-        Tells the windows hooks wrapper to publish user input events or not.
-        :param input_types_to_publish: Flag. Some bitwise combination of the InputTypes enum
-            that represents each type of input and whether to publish user events of that type
-        """
-        self.publishing_user_keyboard_events = (input_types_to_publish & InputTypes.KEYBOARD)
-        self.publishing_user_mouse_button_events = (
-            input_types_to_publish & InputTypes.MOUSE_BUTTONS)
-        self.publishing_user_mouse_move_events = (input_types_to_publish & InputTypes.MOUSE_MOVE)
-
-    def on_keyboard_event(self, event):
-        """
-        Called back from pyHooks library on a keyboard event
-        :param event: event passed from pyHooks
-        :return: True if we are to pass the event on to other hooks and the process it was intended
-         for. False to consume the event.
-        """
-        # When the user presses the escape key, we shall stop consuming events, so they may regain
-        # control in if they got stuck in a bad state where input was being consumed when they did
-        # not expect it?
-        if event.KeyID == win32con.VK_ESCAPE and not event.Injected:
-            consume = self.consuming_user_keyboard_events
-            publish = self.publishing_user_keyboard_events
-            self.set_user_input_types_to_consume(InputTypes.NONE)
-        else:
-            # Event was not user failsafe key event
-            if event.Injected:
-                # Always publish injected events
-                # Do not consume injected events
-                publish = True
-                consume = False
-            else:
-                publish = self.publishing_user_keyboard_events
-                consume = self.consuming_user_keyboard_events
-
-        if publish:
-            if self.window_to_publish_to:
-                from twisted.internet import reactor
-                if event.Message == HookConstants.WM_KEYDOWN:
-                    reactor.callFromThread(self.window_to_publish_to.print_to_text_box,
-                                           HookEvents.key_down.value, event)
-                elif event.Message == HookConstants.WM_KEYUP:
-                    reactor.callFromThread(self.window_to_publish_to.print_to_text_box,
-                                           HookEvents.key_up.value, event)
-        return not consume
-
     def on_mouse_event(self, event):
         """
         Called back from pyHooks library on a mouse event
@@ -120,42 +36,11 @@ class WindowsHooksWrapper(object):
         :return: True if we are to pass the event on to other hooks and the process it was intended
          for. False to consume the event.
         """
-        publish = True
-        consume = False
 
-        if event.Injected:
-            # Always publish injected events
-            # Do not consume injected events
-            publish = True
-            consume = False
-
-        elif event.Message in MOUSE_BUTTON_EVENTS:
-            publish = self.publishing_user_mouse_button_events
-            consume = self.consuming_user_mouse_button_events
-
-        elif event.Message == HookConstants.WM_MOUSEMOVE:
-            publish = self.publishing_user_mouse_move_events
-            consume = self.consuming_user_mouse_move_events
-
-        if publish:
-            if self.window_to_publish_to:
-                from twisted.internet import reactor
-                if event.Message == HookConstants.WM_LBUTTONDOWN:
-                    reactor.callFromThread(self.window_to_publish_to.print_to_text_box,
-                                           HookEvents.mouse_left_down.value, event)
-                elif event.Message == HookConstants.WM_LBUTTONUP:
-                    reactor.callFromThread(self.window_to_publish_to.print_to_text_box,
-                                           HookEvents.mouse_left_up.value, event)
-                elif event.Message == HookConstants.WM_RBUTTONDOWN:
-                    reactor.callFromThread(self.window_to_publish_to.print_to_text_box,
-                                           HookEvents.mouse_right_down.value, event)
-                elif event.Message == HookConstants.WM_RBUTTONUP:
-                    reactor.callFromThread(self.window_to_publish_to.print_to_text_box,
-                                           HookEvents.mouse_right_up.value, event)
-                elif event.Message == HookConstants.WM_MOUSEMOVE:
-                    reactor.callFromThread(self.window_to_publish_to.print_to_text_box,
-                                           HookEvents.mouse_move.value, event)
-        return not consume
+        if self.window_to_publish_to:
+            from twisted.internet import reactor
+            reactor.callFromThread(self.window_to_publish_to.print_to_text_box, event)
+        return True
 
     def thread_proc(self):
         print "Thread started with Id {}".format(threading.current_thread().ident)
@@ -165,8 +50,6 @@ class WindowsHooksWrapper(object):
         # Also note that for exception safety, when the hook manager goes out of scope, the
         #     documentation says that it unregisters all outstanding hooks
         self.hook_manager = pyHook.HookManager()
-        self.hook_manager.KeyAll = self.on_keyboard_event
-        self.hook_manager.HookKeyboard()
         self.hook_manager.MouseAll = self.on_mouse_event
         self.hook_manager.HookMouse()
 
@@ -175,6 +58,5 @@ class WindowsHooksWrapper(object):
 
         print "Thread exiting..."
 
-        self.hook_manager.UnhookKeyboard()
         self.hook_manager.UnhookMouse()
         self.hook_manager = None
